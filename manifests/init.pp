5fba6fec08bf7f53365b9c9705c2a72a1e9b0a7c
Zach Leslie <zach@puppetlabs.com>
2011-08-24 09:58:25 -0700

    use correct package names, add docs, ensure test passes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,28 @@
+# Class: inetd
+#
+# Has been tested on: debian
+#
+# Parameters:
+#
+# Actions:
+# * Installs inetd package
+# * Starts the inetd service
+#
+# Requires:
+#
+# Sample Usage:
+#
+#    include inetd
+#
 class inetd {
 
-  service { "inetd":
+  Package["openbsd-inetd"] -> Service["openbsd-inetd"]
+
+  package { "openbsd-inetd":
+    ensure => installed;
+  }
+
+  service { "openbsd-inetd":
     enable => true,
     ensure => running;
   }
